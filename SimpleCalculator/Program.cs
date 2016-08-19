d3f902aaea826ec1cf0ce7388469c5230391f8a8
Scott Schaffer <scottpschaffer@hotmail.com>
2016-08-18 20:45:41 -0700

    finished Chunk 3

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -14,18 +14,25 @@ namespace SimpleCalculator
             string inp = "";
             int x2 = 0;
             Expression e1 = new Expression();
+            Stack s1 = new Stack();
             //
             do
             {
-                Console.Write("[" + c + "]>");
-                inp = Console.ReadLine();
+                Console.Write("[" + c + "]> ");
+                inp = Console.ReadLine().ToLower();
                 string[] x1 = e1.Extract(inp);
                 if (x1[0] != "Failed")
                 {
                     x2 = e1.Process(x1);
+                    s1.add2Stack(inp, x2.ToString());
+                    Console.WriteLine("   = " + x2.ToString());
                 }
-
-                Console.WriteLine("   =" + x2);
+                
+                if ((inp == "listq") || (inp == "list"))
+                {
+                    string readStack = s1.readFromStack(inp);
+                    Console.WriteLine("   = " + readStack);
+                }
 
                 //Console.WriteLine("First#: " + x1[0] + " Operand: " + x1[1] + " Second#: " + x1[2]);
                 c++;
---
+++ SimpleCalculator/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Stack
+    {
+        List<string> theStack = new List<string>();
+        
+        public void add2Stack(string listq, string list)
+        {
+            theStack.Add(listq);
+            theStack.Add(list);
+        }
+
+        public string readFromStack(string which)
+        {
+            string result = "empty stack";
+            if (theStack.Count > 0)
+            {
+                switch (which)
+                {
+                    case "listq":
+                        result = theStack[theStack.Count - 2];
+                        break;
+                    case "list":
+                        result = theStack[theStack.Count - 1];
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
+        }
+       
+    }
+}
